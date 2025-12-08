dc363c25cde3c4ef418bae924863ec5967636a58
rvmauxclb <rvmauxclb648@gmail.com>
2025-12-08 17:22:29 +0800

    fix_milvus

--- testing-deep-agents-service/src/core/config.py
+++ testing-deep-agents-service/src/core/config.py
@@ -74,6 +74,7 @@ class Settings(BaseSettings):
     class Config:
         case_sensitive = True
         env_file = ".env"
+        extra = "ignore"  # 忽略未定义的环境变量
 
     def validate_configuration(self) -> List[str]:
         """
